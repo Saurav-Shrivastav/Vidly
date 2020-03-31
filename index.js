b8c5adde84c1181b162ea2bfd8e9f7387c6e78f8
Saurav <sauravsrivastav103@gmail.com>
2020-03-31 22:08:37 +0530

    beautified the code'

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-
-const Joi = require('joi');     //Required for input validation.
-const express = require('express');   // Instantiating the express object that returns a function.
+const Joi = require('joi'); //Required for input validation.
+const express = require('express'); // Instantiating the express object that returns a function.
 const app = express();
 
-app.use(express.json());    // To enable parsing of json objects to use the req.body.name in the post and put requests.
+app.use(express.json()); // To enable parsing of json objects to use the req.body.name in the post and put requests.
 
-const genres = [
-  {
+const genres = [{
     id: 1,
     name: 'Action'
   },
@@ -36,22 +34,24 @@ app.get('/api/genres', (req, res) => {
 // GET requests for single genres:
 app.get('/api/genres/:id', (req, res) => {
 
-  const genre = genres.find(c => c.id === parseInt(req.params.id));   // Finding the given genre, by the req.params.id
+  const genre = genres.find(c => c.id === parseInt(req.params.id)); // Finding the given genre, by the req.params.id
 
-  if(genre) res.send(genre);      // If the genre exists then it is sent via the response.
-  
-  else res.status(404).send('Cannot find the genre');   // Else a status of 404 with a message is sent back.
+  if (genre) res.send(genre); // If the genre exists then it is sent via the response.
+
+  else res.status(404).send('Cannot find the genre'); // Else a status of 404 with a message is sent back.
 });
 
 // POST requests:
 app.post('/api/genres', (req, res) => {
   // Validate:
-  const { error } = validateGenre(req.body);
+  const {
+    error
+  } = validateGenre(req.body);
 
   // If invalid, return 400 - BAD request.
-  if(error) return res.status(400).send(result.error.details[0].message);
+  if (error) return res.status(400).send(result.error.details[0].message);
 
-  const genre = {                 // The object that needs to be posted.
+  const genre = { // The object that needs to be posted.
     id: genres.length + 1,
     name: req.body.name
   };
@@ -66,12 +66,14 @@ app.put('/api/genres/:id', (req, res) => {
   const genre = genres.find((c) => c.id === parseInt(req.params.id));
 
   // If it doesn't exist then return a status of 404
-  if(!genre) return res.status(404).send('Cannot find the given genre.');
+  if (!genre) return res.status(404).send('Cannot find the given genre.');
 
   // Validate:
-  const { error } = validateGenre(req.body);
+  const {
+    error
+  } = validateGenre(req.body);
   // If invalid, return 400 - BAD request.
-  if(error) return res.status(400).send(result.error.details[0].message);
+  if (error) return res.status(400).send(result.error.details[0].message);
 
   // Update the genres.
   genre.name = req.body.name;
@@ -86,7 +88,7 @@ app.delete('/api/genres/:id', (req, res) => {
   const genre = genres.find(c => c.id === parseInt(req.params.id));
 
   // If not existing, return 404:
-  if(!genre) return res.status(404).send('The genre doesn\'t exist.');
+  if (!genre) return res.status(404).send('The genre doesn\'t exist.');
 
   // Delete:
   const index = genres.indexOf(genre);
@@ -98,16 +100,16 @@ app.delete('/api/genres/:id', (req, res) => {
 });
 
 // PORT:
-const port = process.env.PORT || 3000;  // the global object 'process' has the env property which has a PORT property which gives the available ports on the server at that time else port-3000 is assigned.
+const port = process.env.PORT || 3000; // the global object 'process' has the env property which has a PORT property which gives the available ports on the server at that time else port-3000 is assigned.
 app.listen(port, () => console.log(`Listening on port ${port}....`));
 // It called when the application starts listening on a particular port.
 
-function validateGenre(genre){
-  const schema = {      // The schema for input validation is defined.
+function validateGenre(genre) {
+  const schema = { // The schema for input validation is defined.
     name: Joi.string()
-    .min(3)
-    .required()
+      .min(3)
+      .required()
   };
 
-  return Joi.validate(genre.body, schema);   // Validates the body of the request with the schema.
+  return Joi.validate(genre.body, schema); // Validates the body of the request with the schema.
 }
