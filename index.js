5e1176067804c148d1db83ae61f8a68724ec217e
Saurav <sauravsrivastav103@gmail.com>
2020-03-31 21:44:36 +0530

    Post and pput requests are completely functional with input validation

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+const Joi = require('joi');     //Required for input validation.
 const express = require('express');   // Instantiating the express object that returns a function.
 const app = express();
 
@@ -40,7 +42,14 @@ app.get('/api/genres/:id', (req, res) => {
 
 // POST requests:
 app.post('/api/genres', (req, res) => {
-  const genre = {
+  const { error } = validateGenre(req.body);
+
+  if(error){       // If there is any error in the validation then the following block is executed.
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  const genre = {                 // The object that needs to be posted.
     id: genres.length + 1,
     name: req.body.name
   };
@@ -48,7 +57,42 @@ app.post('/api/genres', (req, res) => {
   res.send(genre);
 });
 
+// PUT requests:
+app.put('/api/genres/:id', (req, res) => {
+
+  // Look up for the genre
+  const genre = genres.find((c) => c.id === parseInt(req.params.id));
+
+  // If it doesn't exist then return a status of 404
+  if(!genre) return res.status(404).send('Cannot find the given genre.');
+
+  // Validate:
+  const { error } = validateGenre(req.body);
+  // If invalid, return 400 - BAD request.
+  if(error){
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  // Update the genres.
+  genre.name = req.body.name;
+
+  // Return the updated genre.
+  res.send(genre);
+
+});
+
 // PORT:
 const port = process.env.PORT || 3000;  // the global object 'process' has the env property which has a PORT property which gives the available ports on the server at that time else port-3000 is assigned.
 app.listen(port, () => console.log(`Listening on port ${port}....`));
 // It called when the application starts listening on a particular port.
+
+function validateGenre(genre){
+  const schema = {      // The schema for input validation is defined.
+    name: Joi.string()
+    .min(3)
+    .required()
+  };
+
+  return Joi.validate(genre.body, schema);   // Validates the body of the request with the schema.
+}
