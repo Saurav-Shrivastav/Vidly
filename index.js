e38907d2a9bf97fc14d75b89db4fd5bd77b4d351
Saurav <sauravsrivastav103@gmail.com>
2020-04-09 17:28:25 +0530

    Jwt authentication added, also used the config module for setting the env variables on the machine.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
@@ -9,6 +10,11 @@ const users = require('./routes/users.js');
 const auth = require('./routes/auth.js');
 const express = require('express'); // Instantiating the express object that returns a function.
 const app = express();
+
+if (!config.get('jwtPrivateKey')) {
+  console.error('FATAL ERROR: jwtPrivateKey is not defined.');
+  process.exit(1);
+}
 
 mongoose.connect('mongodb://localhost/Vidly', {
     useUnifiedTopology: true,
